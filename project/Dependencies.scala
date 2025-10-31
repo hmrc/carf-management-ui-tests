5cb83b86c5832bc01606af0488ed55d0a7258b0f
anjali.nair <anjalipnair2020@gmail.com>
2025-10-31 07:47:03 +0000

    CARF-230_UpdatingTestRunnerVersion

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,9 @@
-import sbt._
+import sbt.*
 
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "ch.qos.logback"       % "logback-classic" % "1.5.18" % Test,
+    "ch.qos.logback"       % "logback-classic" % "1.5.20" % Test,
     "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8" % Test,
     "org.scalatest"       %% "scalatest"       % "3.2.19" % Test,
     "uk.gov.hmrc"         %% "ui-test-runner"  % "0.50.0" % Test,
