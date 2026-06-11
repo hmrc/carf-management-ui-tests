b6943af701982be2db853e1e9da318b6aa78d4e2
Prashant-digital <180386844+Prashant-digital@users.noreply.github.com>
2026-06-11 17:35:23 +0100

    Removed thread sleep

--- src/test/scala/uk/gov/hmrc/test/ui/specs/ManagementSpec.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/specs/ManagementSpec.scala
@@ -61,12 +61,13 @@ class ManagementSpec extends BaseSpec {
       And("the Individual user enters first name and last name on the '/individual-name' page")
       IndividualNamePage.enterIndName("John", "Doe")
 
-      Thread.sleep(5000) // TODO: Remove once the previous pages are ready and we can navigate through the journey
       And("the Individual user enters NI number on the '/ni-number' page")
       IndividualNiNumberPage.enterNiNumber("PB200807C")
 
       And("the Individual user is taken to '/find-address' page")
       FindAddressPage.onPage()
+
+      Thread.sleep(5000) // TODO: Remove once the previous pages are ready and we can navigate through the journey
       And("the Individual user navigates to '/individual-email' page")
       IndividualEmailPage.indEmailPage // TODO: Update the navigation once previous pages are built; currently it goes directly to /individual-email page
       And("the Individual user enters their email on the '/individual-email' page")
