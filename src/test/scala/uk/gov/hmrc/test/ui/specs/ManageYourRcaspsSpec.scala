156de443e50c8af0611b893b8a2091ab93b2ebfa
Prashant-digital <180386844+Prashant-digital@users.noreply.github.com>
2026-06-25 15:47:22 +0100

    Commented scenario 1 to check with Jill

--- src/test/scala/uk/gov/hmrc/test/ui/specs/ManageYourRcaspsSpec.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/specs/ManageYourRcaspsSpec.scala
@@ -10,22 +10,23 @@ class ManageYourRcaspsSpec extends BaseSpec {
     // 2. Organisation user without CT-UTR enrolment, with RCASPs added
     // 3. Individual with RCASPs added
 
-    Scenario("1 - Organisation user with CT-UTR enrolment, without any RCASPs added", ManagementTests, ZapTests) {
-      Given("the Organisation user logs in with a valid CARF ID and CT UTR")
-      AuthLoginPage.loginAsOrgAdminWithCtUtr("KK110")
-      And("the Organisation user is on '/manage-cryptoasset-reports' page")
-      ServiceHomePage.onPage()
-      Thread.sleep(5000) // TODO: Remove once the previous pages are ready and we can navigate through the journey
-      And("the Organisation user navigates to '/your-rcasps' page and selects 'Yes'")
-      YourRcaspsPage.navigateToYourRcaspsPage // TODO: Remove the method
-      YourRcaspsPage.select("Yes")
-      And("the Organisation user selects 'Yes' in the '/report-for-registered-business' page ")
-      ReportForRegisteredBusiness.select("No")
-      And("the Organisation user selects 'Organisation' on '/organisation-or-individual' page")
-      OrganisationOrIndividualPage.selectRcaspType("Individual")
-      // TODO: Check if need to Continue journey?
-
-    }
+    // TODO: Discuss with Jill if this will be covered in Remove journey
+//    Scenario("1 - Organisation user with CT-UTR enrolment, without any RCASPs added", ManagementTests, ZapTests) {
+//      Given("the Organisation user logs in with a valid CARF ID and CT UTR")
+//      AuthLoginPage.loginAsOrgAdminWithCtUtr("KK110")
+//      And("the Organisation user is on '/manage-cryptoasset-reports' page")
+//      ServiceHomePage.onPage()
+//      Thread.sleep(5000) // TODO: Remove once the previous pages are ready and we can navigate through the journey
+//      And("the Organisation user navigates to '/your-rcasps' page and selects 'Yes'")
+//      YourRcaspsPage.navigateToYourRcaspsPage // TODO: Remove the method
+//      YourRcaspsPage.select("Yes")
+//      And("the Organisation user selects 'No' in the '/report-for-registered-business' page ")
+//      ReportForRegisteredBusiness.select("No")
+//      And("the Organisation user selects 'Organisation' on '/organisation-or-individual' page")
+//      OrganisationOrIndividualPage.selectRcaspType("Individual")
+//      // TODO: Check if need to Continue journey?
+//
+//    }
 
     Scenario("2 - Organisation user without CT-UTR enrolment, with RCASPs added", ManagementTests, ZapTests) {
       Given("the Organisation user logs in with a valid CARF ID")
