37902ec279c549b7b9a105c1b789fbfd06765ce0
Prashant Soni <180386844+Prashant-digital@users.noreply.github.com>
2026-08-03 10:14:58 +0100

    Added script for CARF-351 (#28)
    * Added script for CARF-351
    
    * Formating and resolved comments

---
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/ReportForRegisteredBusinessChangeModePage.scala
@@ -0,0 +1,6 @@
+package uk.gov.hmrc.test.ui.pages
+
+object ReportForRegisteredBusinessChangeModePage extends BasePage {
+  override val pageUrl: String = baseUrl + "/manage-your-rcasps/registered-business/change-report-for-registered-business"
+
+}
